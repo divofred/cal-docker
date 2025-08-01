d6592b478f6a6b82d5231c51da7dcac1c36ddeb3
Fredrick Emmanuel <turbo@m.chigisoft.com>
2025-08-01 10:09:51 +0100

    first commit

---
+++ calcom/packages/features/insights/components/index.ts
@@ -0,0 +1,12 @@
+export { BookedByCell } from "./BookedByCell";
+export { BookingAtCell } from "./BookingAtCell";
+export { BookingStatusBadge } from "./BookingStatusBadge";
+export { CardInsights } from "./Card";
+export { CellWithOverflowX } from "./CellWithOverflowX";
+export { ChartCard } from "./ChartCard";
+export { FeedbackTable } from "./FeedbackTable";
+export { KPICard } from "./KPICard";
+export { LineChart } from "./LineChart";
+export { LoadingInsight } from "./LoadingInsights";
+export { ResponseValueCell } from "./ResponseValueCell";
+export { UserStatsTable } from "./UserStatsTable";
